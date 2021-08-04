c794d8d340cf5134e7e3ffe0c3703225a1571ff7
FF <>
2021-08-04 18:51:49 +0800

    feature(module-statistic): 统计板块的结构搭建

--- src/style/entry.ts
+++ src/style/entry.ts
@@ -29,4 +29,5 @@ import "./components/module-show.scss";
 import "./components/module-contact.scss";
 import "./components/module-intro.scss";
 import "./components/module-doc.scss";
+import "./components/module-statistic.scss";
 import "./components/page-index.scss";
