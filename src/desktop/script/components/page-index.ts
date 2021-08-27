d824dc9110c44ca4386bea55a4dc9d2fce0f8487
FF <>
2021-08-27 13:17:27 +0800

    feature(module-statistic): module-statistic 板块适配

--- src/desktop/script/components/page-index.ts
+++ src/desktop/script/components/page-index.ts
@@ -15,12 +15,12 @@ import {
     getLineEnter,
     getLineEnterEnd,
 } from "./propaganda-module";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 import Impetus from "impetus";
 import initDocModuleScroll from "./doc-module-scroll";
 import initCustomerModuleScroll from "./customer-module-scroll";
 import initShowModuleScroll from "./show-module-scroll";
 import initDesignModuleScroll from "./design-module-scroll";
+import statisticModuleScroll from "./statistic-module-scroll";
 import lottie from "lottie-web";
 
 export default class IndexPage extends SiteManage {
@@ -256,97 +256,7 @@ export default class IndexPage extends SiteManage {
     }
     statisticModule(): void {
         if (!$(".module-statistic").length) return;
-        const $module = $(".module-statistic");
-        const itemInfo = $(".module-statistic .item-info");
-        ScrollTrigger.create({
-            trigger: ".module-statistic .box-intro_img",
-            scrub: true,
-            start: "center center",
-            end: "bottom bottom",
-            endTrigger: ".module-statistic",
-            pin: true,
-        });
-        let oldIndex = -1;
-        const doms = $(".module-statistic .item-intro_img");
-        gsap.set(doms.not(doms.eq(0)), {
-            opacity: 0,
-        });
-        function setSection(currentIndex, dir) {
-            if (dir == 1) {
-                if (currentIndex <= oldIndex) return;
-            } else {
-                if (currentIndex >= oldIndex) return;
-            }
-            if (oldIndex != -1) {
-                gsap.set(doms.eq(oldIndex), {
-                    zIndex: 0,
-                });
-                gsap.to(doms.eq(oldIndex), {
-                    duration: 0.36,
-                    overwrite: "auto",
-                    autoAlpha: 0,
-                });
-            }
-            gsap.set(doms.eq(currentIndex), {
-                zIndex: 10,
-            });
-            gsap.to(doms.eq(currentIndex), {
-                duration: 0.36,
-                overwrite: "auto",
-                autoAlpha: 1,
-            });
-            oldIndex = currentIndex;
-        }
-        itemInfo.each((i, dom) => {
-            const animate = gsap.timeline({
-                defaults: {
-                    ease: "Power2.easeOut",
-                },
-                scrollTrigger: {
-                    start: "top 80%",
-                    end: "bottom 20%",
-                    trigger: dom,
-                    scrub: true,
-                    onUpdate({ progress, direction }) {
-                        if (direction == -1) {
-                            progress <= 0.5 && setSection(i, direction);
-                        } else {
-                            progress >= 0.5 && setSection(i, direction);
-                        }
-                    },
-                },
-            });
-            animate.from(dom, {
-                opacity: () => {
-                    return i == 0 ? 1 : 0.2;
-                },
-            });
-            animate.to(dom, {
-                opacity: 1,
-            });
-            animate.to(dom, {
-                opacity: () => {
-                    return i == itemInfo.length - 1 ? 1 : 0.3;
-                },
-            });
-        });
-        gsap.fromTo(
-            $module.find(".bg-circle"),
-            {
-                opacity: 0,
-                scale: 0.4,
-            },
-            {
-                opacity: 1,
-                scale: 1,
-                stagger: 0.1,
-                ease: "Power2.easeOut",
-                scrollTrigger: {
-                    trigger: $module.find(".box-bg"),
-                    start: "center bottom",
-                },
-            }
-        );
+        statisticModuleScroll();
     }
     extensionModule(): void {
         if (!$(".module-extension").length) return;
---
+++ src/desktop/script/components/statistic-module-scroll.ts
@@ -0,0 +1,270 @@
+/*
+ * @Description:
+ * @Author: F-Stone
+ * @Date: 2021-08-27 12:01:11
+ * @LastEditTime: 2021-08-27 12:01:12
+ * @LastEditors: F-Stone
+ */
+
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+function normalScreen() {
+    const $module = $(".module-statistic");
+    const itemInfo = $(".module-statistic .item-info");
+    ScrollTrigger.create({
+        trigger: ".module-statistic .box-intro_img",
+        scrub: true,
+        start: "center center",
+        end: "bottom bottom",
+        endTrigger: ".module-statistic",
+        pin: true,
+        invalidateOnRefresh: true,
+    });
+    let oldIndex = -1;
+    const doms = $(".module-statistic .item-intro_img");
+    gsap.set(doms.not(doms.eq(0)), {
+        opacity: 0,
+    });
+    function setSection(currentIndex, dir) {
+        if (dir == 1) {
+            if (currentIndex <= oldIndex) return;
+        } else {
+            if (currentIndex >= oldIndex) return;
+        }
+        if (oldIndex != -1) {
+            gsap.set(doms.eq(oldIndex), {
+                zIndex: 0,
+            });
+            gsap.to(doms.eq(oldIndex), {
+                duration: 0.36,
+                overwrite: "auto",
+                autoAlpha: 0,
+            });
+        }
+        gsap.set(doms.eq(currentIndex), {
+            zIndex: 10,
+        });
+        gsap.to(doms.eq(currentIndex), {
+            duration: 0.36,
+            overwrite: "auto",
+            autoAlpha: 1,
+        });
+        oldIndex = currentIndex;
+    }
+    itemInfo.each((i, dom) => {
+        const animate = gsap.timeline({
+            defaults: {
+                ease: "Power2.easeOut",
+            },
+            scrollTrigger: {
+                start: "top 80%",
+                end: "bottom 20%",
+                trigger: dom,
+                scrub: true,
+                onUpdate({ progress, direction }) {
+                    if (direction == -1) {
+                        progress <= 0.5 && setSection(i, direction);
+                    } else {
+                        progress >= 0.5 && setSection(i, direction);
+                    }
+                },
+            },
+        });
+        animate.from(dom, {
+            opacity: () => {
+                return i == 0 ? 1 : 0.2;
+            },
+        });
+        animate.to(dom, {
+            opacity: 1,
+        });
+        animate.to(dom, {
+            opacity: () => {
+                return i == itemInfo.length - 1 ? 1 : 0.3;
+            },
+        });
+    });
+    gsap.fromTo(
+        $module.find(".bg-circle"),
+        {
+            opacity: 0,
+            scale: 0.4,
+        },
+        {
+            opacity: 1,
+            scale: 1,
+            stagger: 0.1,
+            ease: "Power2.easeOut",
+            scrollTrigger: {
+                trigger: $module.find(".box-bg"),
+                start: "center bottom",
+            },
+        }
+    );
+}
+function phoneScreen() {
+    const $module = $(".module-statistic");
+    const itemInfo = $(".module-statistic .item-info");
+    const scrollAnim = gsap.timeline({
+        scrollTrigger: {
+            trigger: ".module-statistic .state-pos_right",
+            scrub: true,
+            start: "center center",
+            end: () => "+=" + window.outerHeight * (itemInfo.length / 2),
+            pin: ".module-statistic .module-body",
+            invalidateOnRefresh: true,
+        },
+    });
+    let oldIndex = -1;
+    const doms = $(".module-statistic .item-intro_img");
+    gsap.set(doms.not(doms.eq(0)), {
+        opacity: 0,
+    });
+    function setSection(currentIndex) {
+        const dir = scrollAnim.scrollTrigger.direction;
+        if (dir == 1) {
+            if (currentIndex <= oldIndex) return;
+        } else {
+            if (currentIndex >= oldIndex) return;
+        }
+        if (oldIndex != -1) {
+            gsap.set(doms.eq(oldIndex), {
+                zIndex: 0,
+            });
+            gsap.to(doms.eq(oldIndex), {
+                duration: 0.36,
+                overwrite: "auto",
+                autoAlpha: 0,
+            });
+        }
+        gsap.set(doms.eq(currentIndex), {
+            zIndex: 10,
+        });
+        gsap.to(doms.eq(currentIndex), {
+            duration: 0.36,
+            overwrite: "auto",
+            autoAlpha: 1,
+        });
+        oldIndex = currentIndex;
+    }
+    const distance = "10%";
+    itemInfo.each((i, dom) => {
+        gsap.set(dom, { y: distance });
+        if (i == 0) {
+            scrollAnim.to(dom, {
+                duration: 1,
+                y: 0,
+                zIndex: 10,
+                onComplete() {
+                    setSection(i);
+                },
+            });
+            scrollAnim.to(
+                dom,
+                {
+                    opacity: 1,
+                    ease: "none",
+                    duration: 0.8,
+                },
+                "<"
+            );
+            scrollAnim.to(dom, {
+                duration: 1,
+                y: "-" + distance,
+                zIndex: 0,
+            });
+            scrollAnim.to(
+                dom,
+                {
+                    opacity: 0,
+                    ease: "none",
+                    duration: 0.8,
+                },
+                "-=0.8"
+            );
+        } else if (i == itemInfo.length - 1) {
+            scrollAnim.to(dom, {
+                duration: 1,
+                y: 0,
+                zIndex: 10,
+                onStart() {
+                    setSection(i);
+                },
+                onReverseComplete() {
+                    setSection(i - 1);
+                },
+            });
+            scrollAnim.to(
+                dom,
+                {
+                    opacity: 1,
+                    ease: "none",
+                    duration: 0.8,
+                },
+                "<"
+            );
+        } else {
+            scrollAnim.to(dom, {
+                duration: 1,
+                y: 0,
+                zIndex: 10,
+                onStart() {
+                    setSection(i);
+                },
+                onReverseComplete() {
+                    setSection(i - 1);
+                },
+            });
+            scrollAnim.to(
+                dom,
+                {
+                    opacity: 1,
+                    ease: "none",
+                    duration: 0.8,
+                },
+                "<"
+            );
+            scrollAnim.to(dom, {
+                duration: 1,
+                y: "-" + distance,
+                zIndex: 0,
+            });
+            scrollAnim.to(
+                dom,
+                {
+                    opacity: 0,
+                    ease: "none",
+                    duration: 0.8,
+                },
+                "-=0.8"
+            );
+        }
+    });
+    scrollAnim.to({}, { duration: 2 });
+    gsap.fromTo(
+        $module.find(".bg-circle"),
+        {
+            opacity: 0,
+            scale: 0.4,
+        },
+        {
+            opacity: 1,
+            scale: 1,
+            stagger: 0.1,
+            ease: "Power2.easeOut",
+            scrollTrigger: {
+                trigger: ".module-statistic .state-pos_right",
+                start: "center center",
+            },
+        }
+    );
+}
+export default function statisticModuleScroll(): void {
+    const itemInfo = $(".module-statistic .item-info");
+    ScrollTrigger.saveStyles(itemInfo);
+    ScrollTrigger.matchMedia({
+        "(min-width: 735px)": normalScreen,
+        "(max-width: 734px)": phoneScreen,
+    });
+}
